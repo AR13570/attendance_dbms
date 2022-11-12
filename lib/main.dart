03952b176bed27dd9d89d814d3a6067b8487dfea
ayyanpasha <ayyanpasha_2014@yahoo.com>
2022-11-12 11:29:39 +0530

    Made Functions for Interacting with DB with students,marks,attendance and Subjects.

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           primarySwatch: Colors.blue,
         ),
-        home: HomePage()
+        home: LoginDemo()
         //const LoginDemo(),
         );
   }
--- lib/mongodb.dart
+++ lib/mongodb.dart
@@ -3,6 +3,9 @@ import 'dart:developer';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDatabase {
+  // var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+
+  //Login
   static Future<bool> loginpage(user,pass) async {
     var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
     await db.open();
@@ -24,4 +27,157 @@ class MongoDatabase {
       return true;
     }
   }
+
+  //Add Subjects in DB
+  static Future<bool> addsubject(subject) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Subject');
+    // Fluent way
+    var data = await coll.find({'subject': subject,'teacher': teacher}).toList();
+    if(data.isEmpty)
+    {
+      await coll.insertOne({
+        'subject': subject,
+        'teacher': teacher
+      });
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  //Add Students into DB
+  static Future<bool> addstudent(student) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Student');
+    // Fluent way
+    var data = await coll.find({'student': student,'teacher': teacher}).toList();
+    if(data.isEmpty)
+    {
+      await coll.insertOne({
+        'student': student,
+        'teacher': teacher
+      });
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  //Add Attendance into DB
+  static Future<bool> addattendannce(date,studentpresent) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Attendance');
+    //Add date and teacher into studentpresent list
+    for(int i=0;i<studentpresent.length();i++)
+    {
+      studentpresent[i]['date'] = date;
+      studentpresent[i]['teacher'] = teacher;
+    }
+    // Fluent way
+    var data = await coll.find({'date': date,'teacher': teacher}).toList();
+    if(!data.isEmpty)
+    {
+      await coll.deleteMany({'date': date,'teacher': teacher});
+    }
+    await coll.insertMany(studentpresent);
+    return true;
+  }
+
+  //Add Marks into DB
+  static Future<bool> addmarks(exam,studentmarks) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Scores');
+    //Add date and teacher into studentmarks list
+    for(int i=0;i<studentmarks.length();i++)
+    {
+      studentmarks[i]['exam'] = exam;
+      studentmarks[i]['teacher'] = teacher;
+    }
+    // Fluent way
+    var data = await coll.find({'exam': exam,'teacher': teacher}).toList();
+    if(!data.isEmpty)
+    {
+      await coll.deleteMany({'exam': exam,'teacher': teacher});
+    }
+    await coll.insertMany(studentmarks);
+    return true;
+  }
+
+  //Get Student List from DB
+  static Future<List<Map<String,dynamic>>> getstudent() async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Student');
+    // Fluent way
+    var data = await coll.find({'teacher': teacher}).toList();
+    return data;
+  }
+
+  //Get Marks List from DB
+  static Future<List<Map<String,dynamic>>> getmarks(subject) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Scores');
+    // Fluent way
+    var data = await coll.find({'teacher': teacher, 'subject': subject}).toList();
+    return data;
+  }
+
+  //Get Subject List from DB
+  static Future<List<Map<String,dynamic>>> getmarks(subject) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Subject');
+    // Fluent way
+    var data = await coll.find({'teacher': teacher}).toList();
+    return data;
+  }
+
+  //Get Attendance List from DB
+  static Future<List<Map<String,dynamic>>> getattendance(date) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    var coll = db.collection('Attendance');
+    // Fluent way
+    var data = await coll.find({'teacher': teacher, 'date': date}).toList();
+    return data;
+  }
+
+  //Get Detail of individual Student's Attendance and Marks information
+  static Future<List<Map<String,dynamic>>> getstudentperformance(student) async {
+    var teacher = "Teacher";
+    var db = await Db.create("mongodb+srv://dbms:dbms@cluster0.txlqt8w.mongodb.net/School?retryWrites=true&w=majority");
+    await db.open();
+    inspect(db);
+    //Get Attendance
+    var coll1 = db.collection('Attendance');
+    var data1 = await coll.find({'student': student}).toList();
+    //Get Marks
+    var coll2 = db.collection('Scores');
+    var data2 = await coll.find({'student': student}).toList();
+    //Return
+    return [data1,data2];
+  }
+
 }
